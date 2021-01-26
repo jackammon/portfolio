0364e444464020d1c00bab0f36d29b8a0c736e58
Jack Ammon <jammon17@georgefox.edu>
2021-01-26 09:22:26 -0800

    added google analytics & metadata

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'G-T5GW032EY1'
+})
 
 
 new Vue({
