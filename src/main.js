27f47a3f697e05ead64098db2e533180ad1384ed
Jack Ammon <jammon17@georgefox.edu>
2021-02-09 12:58:29 -0800

    attempt 2 at fix

--- src/main.js
+++ src/main.js
@@ -5,13 +5,12 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import VueAnalytics from 'vue-analytics'
 
-Vue.config.productionTip = false
-
 Vue.use(VueAnalytics, {
   id: 'G-T5GW032EY1',
   router
-});
+})
 
+Vue.config.productionTip = false
 
 new Vue({
   router,
