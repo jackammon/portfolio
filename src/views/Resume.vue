87aa13f7b36b13102adf86e4c64fe4700fcc5400
Jack Ammon <jammon17@georgefox.edu>
2024-04-11 17:56:22 -0700

    Updating resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,37 +3,35 @@
     
       <v-container class="p-fix"> 
             <v-row class="name"> Jack Ammon </v-row>
-            <v-row class ="sub-header"> Aspiring Full Stack Software Engineer </v-row>
-            <v-row class="text"> A natural-born servant leader who builds teams to solve problems. Searches for the treasure in others, bringing the best out to achieve a shared goal. A skilled communicator who knows how to translate technical ideas into language that is understandable and approachable. </v-row>
+            <v-row class ="sub-header"> Full Stack Software Engineer </v-row>
+            <v-row class="text"> Innovative and strategic thinker, with a Computer Science foundation and experience bringing software products to launch. Blending technical insight with a keen eye for business and design, I excel in bringing clarity, direction, and alignment, passionately driving teams towards unified objectives. </v-row>
 
             <v-row style="padding-top: 16px;">
                 <!-- left slide -->
-                <v-col>
-                    <v-row class="section-header"> Contact </v-row>
-                    <v-row> <a> 509.808.1219 </a> </v-row>
-                    <v-row> <a href="mailto:jammon17@georgefox.edu"> jammon17@georgefox.edu </a> </v-row>
-                    <v-row> <a href="https://github.com/jackammon"> github.com/jackammon </a> </v-row>
-                    <v-row> <a href="http://www.linkedin.com/in/jackammon"> linkedin.com/in/jackammon </a> </v-row>
+                <v-col cols="5">
+                    <v-row class="section-header">Contact</v-row>
+                    <v-row> <a class="text">&#x35;&#x30;&#x39;&#x2e;&#x38;&#x30;&#x38;&#x2e;&#x31;&#x32;&#x31;&#x39;</a></v-row>
+                    <v-row> <a class="text" href="mailto:jcarunner@gmail.com"> &#x6a;&#x63;&#x61;&#x72;&#x75;&#x6e;&#x6e;&#x65;&#x72;&#x40;&#x67;&#x6d;&#x61;&#x69;&#x6c;&#x2e;&#x63;&#x6f;&#x6d; </a> </v-row>
+                    <v-row> <a class="text" href="https://github.com/jackammon"> github.com/jackammon </a> </v-row>
+                    <v-row> <a class="text" href="http://www.linkedin.com/in/jackammon"> linkedin.com/in/jackammon </a> </v-row>
 
                     <v-row class="section-header" style="padding-top: 16px;"> Skills </v-row>
-                    <v-row class="sub-titles" style="padding-top: 4px;"> Hard </v-row>
+                    <v-row class="titles"> Hard </v-row>
 
-                    <v-row class="text"> - Frontend: Javascript, Vue.JS, HTML, CSS </v-row>     
-                    <v-row class="text"> - Backend: Java, C/C++, C#, Python, SQL </v-row>
+                    <v-row class="text"> - Frontend: TypeScript, Vue.JS, Angular </v-row>     
+                    <v-row class="text"> - Backend: NET, Node.js, Laravel </v-row>
                     <v-row class="text"> - Git, Docker, Unix </v-row>
-                    <v-row class="text"> - OO & Functional programming </v-row>
+                    <v-row class="text"> - OO programming </v-row>
                     
-                    <v-row class="sub-titles" style="padding-top: 8px;"> Soft </v-row>
+                    <v-row class="titles" style="padding-top: 8px;"> Soft </v-row>
                     <v-row class="text"> - Writing and Speaking </v-row>
                     <v-row class="text"> - Team-Building </v-row>
                     <v-row class="text"> - Coordinating and delegating tasks </v-row>
                     <v-row class="text"> - Decision making </v-row>
 
                     <v-row class="section-header" style="padding-top: 16px;"> Education </v-row>
-                    <v-row class="titles" style="margin-top: -4px !important;"> George Fox University </v-row>
-                    <v-row class="text1" style="margin-top: -4px !important;"> Newberg, Oregon </v-row>
-                    <v-row class="text2"> B.S. Computer Science, April 2021 </v-row>
-
+                    <v-row class="titles"> B.S. Computer Science </v-row>
+                    <v-row class="text2"> @ George Fox University, 2021 </v-row>
                     <v-row class="p-title" style="padding-top: 16px;"> Relevant Courses </v-row>
                     <v-row class="i-text"> - Data Structures </v-row>
                     <v-row class="i-text"> - Object Oriented Design & Analysis </v-row>
@@ -51,9 +49,53 @@
                 <!-- right side -->
                 <v-col>
                     <v-row class="section-header"> Experience </v-row>
-                    <v-row class="titles" > Freelance Web Developer </v-row>
-                    <v-row class="text2" style="margin-top: -4px !important;"> @ Kingdom Web Development LLC </v-row>
-                    <v-row class="text3"> May 2019-December 2020 </v-row>
+                    
+                    <v-row class="titles"> Software Engineering Manager </v-row>
+                    <v-row class="text3"> June 2022 - March 2024 @ Buildable</v-row>
+
+                    <v-row>
+                        <b>30% - Management</b>
+                        <ul>
+                            <li>Led a team of 5 direct report engineers working on multiple projects.</li>
+                            <li>Made resource allocation decisions for weekly company production.</li>
+                            <li>Translated designs into actionable user stories and with LOE engineering estimations.</li>
+                            <li>1-1's with team members, focusing on their goals and career development.</li>
+                        </ul>
+                    </v-row>
+
+                    <v-row>
+                        <b>70% - Engineering</b>
+                        <ul>
+                            <li>Wrote performant front-end components in Vue and Angular.</li>
+                            <li>Wrote performant .NET and Laravel backends.</li>
+                            <li>Setup and maintained production AWS & Azure environments for both internal and client facing projects.</li>
+                            <li>Setup GitLab CI/CD pipelines.</li>
+                            <li>Led daily standups as the scrum master for my projects.</li>
+                        </ul>
+                    </v-row>
+
+                    <v-row class="titles"> Software Engineer </v-row>
+                    <v-row class="text3"> July 2021 - June 2022 @ Buildable</v-row>
+                    <v-row>
+                        <ul>
+                            <li>Successfully developed and launched a custom ERP system with 4 apps and a central server.</li>
+                            <li>Rebuilt client trust and relationship, identifying true remaining project scope and seeing a 6-year-old project to completion and successful launch.<br></li>
+                            <li>Wrote performant front-end components in Vue and Angular.</li>
+                            <li>Wrote performant Laravel and NodeJS backends.</li>
+                            <li>Setup and maintained production AWS environments for both internal and client facing projects.</li>
+                            <li>Integrated with MariaDB and Postgres databases with SQL and Sequelize.</li>
+                            <li>Integrated 3rd party APIs including: Stripe, QBO, Twilio, Zoho, Azure AD SSO.</li>
+                        </ul>
+                    </v-row>
+
+                    <v-row class="titles" > Freelance Software Developer </v-row>
+                    <v-row class="text3"> May 2019-December 2020 @ Kingdom Web Development LLC </v-row>
+                    <ul>
+                        <li>Wrote performant front-end components in Vue.</li>
+                        <li>Wrote performant NodeJS backends.</li>
+                        <li>Integrated Stripe into ecommerce sites.</li>
+                        <li>Worked directly with clients and designers on implementing and iterating on features.</li>
+                    </ul>
 
                     <v-row class="titles" style="padding-top: 8px !important;"> Technical Manager </v-row>
                     <v-row class="text2" style="margin-top: -4px !important;"> @ Servant Engineering & Senior Design Projects for George Fox University </v-row>
@@ -61,21 +103,22 @@
 
                     <v-row class="p-title" style="padding-top: 16px !important;"> UP Movement </v-row>
                     <v-row class="dates"> Fall 2019-Fall 2020 </v-row>
-                    <v-row class="text"> - Led a team of four building a full-stack web app from scratch </v-row>
-                    <v-row class="text"> - Built the frontend in Vue.JS and the backend with C# and Docker. </v-row>
-                    <v-row class="text"> - Communicated project requirements and design ideas with the client. </v-row>
-                    <v-row class="text"> - Worked directly with a creative agency in an iterative development process. </v-row>
-                    <v-row class="text"> - Integrated Stripe.JS to handle donations. </v-row>
-
-
+                    <ul>
+                        <li>Led a team of four building a full-stack web app from scratch</li>
+                        <li>Built the frontend in Vue.JS and the backend with C# and Docker.</li>
+                        <li>Communicated project requirements and design ideas with the client.</li>
+                        <li>Worked directly with a creative agency in an iterative development process.</li>
+                        <li>Integrated Stripe.JS to handle donations.</li>
+                    </ul>
                     <v-row class="p-title" style="padding-top: 16px !important;"> Access Solutions </v-row>
                     <v-row class="dates"> Fall 2020-Spring 2021 </v-row>
-                    <v-row class="text"> - Led a team of six to design and build a comprehensive scheduling system. </v-row>
-                    <v-row class="text"> - Ensured different phases of design and development progressed and integrated smoothly together.  </v-row>
-                    <v-row class="text"> - Led weekly sprint meetings. </v-row>
-                    <v-row class="text"> - Gave presentations to my advisors and clients. </v-row>
-                    <v-row class="text"> - Built the frontend in Vue.JS and the backend with MySQL and Django in a Docker container.  </v-row>
-
+                    <ul>
+                        <li>Led a team of six to design and build a comprehensive scheduling system.</li>
+                        <li>Ensured different phases of design and development progressed and integrated smoothly together.</li>
+                        <li>Led weekly sprint meetings.</li>
+                        <li>Gave presentations to my advisors and clients.</li>
+                        <li>Built the frontend in Vue.JS and the backend with MySQL and Django in a Docker container.</li>
+                    </ul>
                 </v-col>
             </v-row>
       </v-container>
@@ -92,33 +135,35 @@
     .resume {
         font-family: 'Rubik' !important;
         text-align: left;
+        max-width: 850px;
+        margin: auto;
     }
 
     .name {
-        font-weight: 900 !important;
-        font-size: 42pt !important;
+        font-weight: 800 !important;
+        font-size: 32pt !important;
         line-height: 1.2em;
     }
 
     .sub-header {
         text-align: left;
-        font-weight: 900 !important;
-        font-size: 28pt !important;
+        font-weight: 800 !important;
+        font-size: 20pt !important;
     }
 
     .text {
         text-align: left;
-        font-size: 14pt !important;
+        font-size: 11pt !important;
     }
 
     .i-text {
         text-align: left;
-        font-size: 14pt !important;
+        font-size: 11pt !important;
         font-style: italic;
     }
 
     .p-title {
-        font-size: 18pt !important;
+        font-size: 14pt !important;
         font-weight: 800 !important;
     }
 
@@ -127,45 +172,49 @@
     }
 
     .section-header {
-        font-size: 30pt !important;
-        font-weight: 900 !important;
+        font-size: 22pt !important;
+        font-weight: 800 !important;
         text-decoration-line: underline;
     }
 
     .titles {
-        font-size: 24pt !important;
+        font-size: 20pt !important;
         font-weight: 800 !important;
+        text-wrap: nowrap; 
+        margin-top: 6px !important;
     }
 
     .sub-titles {
-        font-size: 22pt !important;
-        font-weight: 900 !important;
+        font-size: 18pt !important;
+        font-weight: 800 !important;
+        margin-top: 6px !important;
     }
 
     .text1 {
-        font-size: 18pt !important;
+        font-size: 16pt !important;
         font-weight: 500 !important;
     }
 
     .text2 {
-        font-size: 16pt !important;
+        font-size: 12pt !important;
         font-weight: 400 !important;
+        margin-top: 8px !important;
     }
 
     .text3 {
-        font-size: 14pt !important;
+        font-size: 12pt !important;
         font-weight: 400 !important;
-        font-style: italic;
+        margin-top: 6px;
     }
 
     .p-fix {
         margin-right: auto;
         margin-left: auto;
-        max-width: 950px;
+        /* max-width: 950px; */
     }
 
     .dates {
-        font-size: 14pt !important;
+        font-size: 12pt !important;
         font-weight: 800 !important;
     }
 
