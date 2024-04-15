d657caeb230b322efac7dbb9e0631b98c98807cf
Jack Ammon <jammon17@georgefox.edu>
2024-04-15 10:16:29 -0700

    Changed baseURL config to /

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
+  base: '/',
   plugins: [
     Vue({
       template: { transformAssetUrls }
