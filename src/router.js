6d1fef9e3f81625be439b90f5d49e05b5316dfa2
Jack Ammon <jammon17@georgefox.edu>
2024-04-16 15:37:38 -0700

    Added redirect for undefined routes

--- src/router.js
+++ src/router.js
@@ -32,6 +32,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About
+  },
+  {
+    // Redirect any unmatched paths to root
+    path: '/:pathMatch(.*)*',
+    redirect: '/'
   }
 ]
 
