476debb55631009c0355e8061666988f35414070
Jack Ammon <jammon17@georgefox.edu>
2024-04-16 22:41:08 -0700

    Added mobile friendliness

--- src/App.vue
+++ src/App.vue
@@ -15,43 +15,35 @@
       >
         <v-tab class="jck-size" to="/" :value="1" :class="tabClass(1)">JCK</v-tab>
         <v-spacer style="width: 70%"></v-spacer>
-        <v-tab class="btn-size" to="/about" :value="2" :class="tabClass(2)">About</v-tab>
-        <!-- <v-tab class="btn-size" to="/projects" :value="3" :class="tabClass(3)">Experience</v-tab> -->
-        <v-tab class="btn-size" to="/contact" :value="4" :class="tabClass(4)">Contact</v-tab>
-        <v-tab class="btn-size" to="/resume" :value="5" :class="tabClass(5)">Resume</v-tab>
+        <v-tab class="btn-size hidden-sm-and-down" to="/about" :value="2" :class="tabClass(2)">About</v-tab>
+        <v-tab class="btn-size hidden-sm-and-down" to="/contact" :value="4" :class="tabClass(4)">Contact</v-tab>
+        <v-tab class="btn-size hidden-sm-and-down" to="/resume" :value="5" :class="tabClass(5)">Resume</v-tab>
       </v-tabs>
+      <v-btn icon class="hidden-md-and-up" @click="drawer = !drawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
     </v-app-bar>
 
+    <!-- Navigation Drawer -->
+    <v-navigation-drawer v-model="drawer" location="right" app>
+      <v-list>
+        <v-list-item v-for="(item, i) in menuItems" :key="i" link :href="item.link">
+          <v-list-item-content>
+            <v-list-item-title class="text-left btn-size">{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main class="pa-0 fill-height">
-      <v-container
-        class="fill-height"
-        fluid
-        style="padding-top: 56px; max-width: 100%"
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
+      <v-container class="fill-height" fluid style="padding-top: 56px;">
+        <v-row align="center" justify="center">
           <v-col class="text-center">
             <router-view/>  
           </v-col>
         </v-row>
       </v-container>
     </v-main>
-
-    <!-- FAB -->
-    <!-- <v-btn
-      icon
-      variant="text"
-      color="black"
-      href="https://github.com/jackammon"
-      target="_blank"
-      class="mb-12 mr-2"
-      location="bottom end"
-      position="fixed"
-    >
-      <v-icon>mdi-github</v-icon>
-    </v-btn> -->
 
     <v-btn
       icon
@@ -74,6 +66,14 @@
 import { ref, onMounted } from 'vue';
 
 const currentTab = ref(1);
+const drawer = ref(false);
+
+const menuItems = [
+  { title: 'Home', link: '/' },
+  { title: 'About', link: '/about' },
+  { title: 'Contact', link: '/contact' },
+  { title: 'Resume', link: '/resume' }
+];
 
 function tabClass(value) {
   return currentTab.value === value ? 'active-tab' : 'inactive-tab';
@@ -81,9 +81,9 @@ function tabClass(value) {
 
 onMounted(() => {
   console.log("Hi there 👋");
-  console.log("I built this website using Vue.js");
-  console.log("If you'd like to get in contact with me my information is found under the contact page");
-  console.log("Have a blessed day!");
+  console.log("I built this website using Vue3, Vuetify, Vite, and GSAP");
+  console.log("If you'd like to get in contact with me, my information is found under the contact page");
+  console.log("Thanks for stopping by!");
 });
 
 </script>
@@ -108,6 +108,17 @@ onMounted(() => {
   font-weight: 700 !important;
   font-family: 'Rubik Mono One';
   letter-spacing: -2.755pt !important;
+}
+
+@media (max-width: 600px) {
+  .btn-size {
+    font-size: 12pt !important;
+  }
+
+  .jck-size {
+    text-align: left !important;
+    justify-content: flex-start !important;
+  }
 }
 
 .active-tab {
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,10 +4,10 @@
       <v-col cols="3"> </v-col>
       <v-col cols="5">
         <div class="effect">
-          <div class="title">
-              <div class="title-word">You can reach me at </div>
-              <div class="title-word">&#x6a;&#x63;&#x61;&#x72;&#x75;&#x6e;&#x6e;&#x65;&#x72;&#x40;&#x67;&#x6d;&#x61;&#x69;&#x6c;&#x2e;&#x63;&#x6f;&#x6d;</div>
-              <div class="title-word">&#x35;&#x30;&#x39;&#x2e;&#x38;&#x30;&#x38;&#x2e;&#x31;&#x32;&#x31;&#x39;</div>
+          <div class="contact">
+              <div>You can reach me at </div>
+              <div>&#x6a;&#x63;&#x61;&#x72;&#x75;&#x6e;&#x6e;&#x65;&#x72;&#x40;&#x67;&#x6d;&#x61;&#x69;&#x6c;&#x2e;&#x63;&#x6f;&#x6d;</div>
+              <div>&#x35;&#x30;&#x39;&#x2e;&#x38;&#x30;&#x38;&#x2e;&#x31;&#x32;&#x31;&#x39;</div>
           </div>
         </div>
       </v-col>
@@ -24,19 +24,35 @@ body {
   justify-content: center;
   background: #262626;
 }
-.title {
+.contact {
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  perspective: 400;
   padding-left: 17px;
   font-family: 'Rubik' !important;
-  font-weight: 900 !important;
-  font-size: 28pt !important;
+  font-weight: 800 !important;
+  font-size: 28pt;
   line-height: 1.2em;
   text-transform: uppercase;
   color: black;
+}
+
+/* Responsive adjustments for tablets */
+@media (max-width: 768px) {
+  .contact {
+    font-size: 18pt; /* smaller font size for tablets */
+  }
+}
+
+/* Responsive adjustments for mobile phones */
+@media (max-width: 480px) {
+  .title {
+    font-size: 8pt; /* even smaller font size for mobile phones */
+    top: 40%; /* adjust position slightly for better viewing */
+    left: 50%;
+    transform: translate(-50%, -40%);
+  }
 }
 
 </style>
