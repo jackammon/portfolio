a1b1e9f9f086104d8d8cc6dba75c8f23802783e5
Jack Ammon <jammon17@georgefox.edu>
2024-04-19 13:02:28 -0700

    added GA

--- src/main.js
+++ src/main.js
@@ -9,10 +9,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import VueGtag from "vue-gtag";
 
 const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
+app.use(VueGtag, {
+  config: { id: "G-8WB4KTLKDJ" } // public key
+})
 
 app.mount('#app')
